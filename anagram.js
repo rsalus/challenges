5c4ba43da7c2b1bf9738d79da766551138e5895f
rsalus <51675276+rsalus@users.noreply.github.com>
2021-10-06 00:55:32 -0400

    Added anagram

---
+++ anagram.js
@@ -0,0 +1,15 @@
+/* https://leetcode.com/problems/valid-anagram/ */
+/* O(n log n) Runtime: 96 ms, faster than 64.41% */
+/* O(n) Memory Usage: 44 MB, less than 30.65% */
+
+function anagram(s, t) {
+    const equals = (a, b) =>
+        a.length === b.length &&
+        a.every((val, i) => val === b[i]);
+    
+    return equals([...s].sort(), [...t].sort());
+}
+
+console.log(
+    anagram("anagram", "nagaram")
+);
--- buySellStock.js
+++ buySellStock.js
@@ -1,1 +1,20 @@
 /* https://leetcode.com/problems/best-time-to-buy-and-sell-stock/ */
+/* O(n) Runtime: 175 ms, faster than 10.20% */
+/* O(1) Memory Usage: 48.2 MB, less than 97.22% */
+
+function buySellStock(prices) {
+    let min = Number.MAX_VALUE;
+    let max = 0;
+
+    for(let i = 0; i < prices.length; i++){
+        if(prices[i] < min)
+            min = prices[i];
+        else    /* calculate price for current day against curr max */
+            max = Math.max(max, prices[i] - min);
+    }
+    return max;
+}
+
+console.log(
+    buySellStock([7, 1, 5, 3, 6, 4])
+);
