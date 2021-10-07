6e828f55cdf06f40dbb90093eb83f3e846c2f1ea
rsalus <51675276+rsalus@users.noreply.github.com>
2021-10-07 00:07:06 -0400

    Modified maxSubArray edge case

--- maxSubArray.js
+++ maxSubArray.js
@@ -1,17 +1,18 @@
 /* https://leetcode.com/problems/maximum-subarray/ */
 /* O(n) Runtime: 84 ms, faster than 66.66% */
-/* O(n) Memory Usage: 48.7 MB, less than 16.86% */
+/* O(n) Memory Usage: 48.6 MB, less than 16.86%*/
 
 function maxSubArray(array) {
-    let curr = 0, largest = array[0];
+    let curr = 0, largest = 0;
 
     for(let number of array){
         curr = Math.max(0, (curr + number));
         largest = Math.max(largest, curr);
     }
 
-    if(largest == 0){
+    if(largest <= 0){
         if(allNegative(array)){
+            largest = array[0];
             for(let number of array)
                 largest = Math.max(largest, number);
         }
@@ -38,5 +39,5 @@ function maxSubArray(arr) {
 */
 
 console.log(
-    maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+    maxSubArray([4, 5, 3, -4, 5, -8, -1])
 );
